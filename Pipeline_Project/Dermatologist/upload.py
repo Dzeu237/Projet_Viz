4b6a67b2fa0fd185befe3b9aa81f3c141dcbe0c2
Dzeu237 <113556789+Dzeu237@users.noreply.github.com>
2025-10-08 12:53:21 +0200

    Destruction

--- Pipeline_Project/Dermatologist/upload.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-#Upload de fichiers
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-else:
-    st.write("No file uploaded")
