c954fe43da6db542b7c30c998695cde0acba8a19
Dzeu237 <113556789+Dzeu237@users.noreply.github.com>
2025-11-03 16:02:09 +0100

    Update app.py

--- Projet_novembre2025/Projets/app.py
+++ Projet_novembre2025/Projets/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # --- Configuration de la page Streamlit ---
 st.set_page_config(
     page_title="Plateforme d'Expérimentation NLP",
-    page_icon=":robot_face:",
+    page_icon="💼",
     layout="wide",
     initial_sidebar_state="expanded"
 )
